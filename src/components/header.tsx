daa5b2193a586c84e2308189718e60b23e193ca9
Jordan Pacheco <jordanrune@gmail.com>
2025-08-03 20:57:20 -0300

    fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='container mx-auto flex h-14 items-center justify-between px-4 md:px-6'>
-        <nav className='flex items-center space-x-6 font-medium text-xs md:text-sm'>
+        <nav className='flex items-center space-x-4 font-medium text-xs md:text-sm'>
           <Link
             activeProps={{
               className: 'text-foreground'
